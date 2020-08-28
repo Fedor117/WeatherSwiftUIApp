27c83c58b520d260d7be1cb604549a9b617fc677
Fedor Valiavko <fedor.valiavko@gmail.com>
2020-08-28 16:24:49 +0100

    Initial Commit

---
+++ WeatherSwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherSwiftUIApp
+//
+//  Created by Fedor Valiavko on 28/08/2020.
+//  Copyright © 2020 Fedor Valiavko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
